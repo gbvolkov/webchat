1218cf6863a1f71a331e2adc90e1d423ce50515b
GBV <gbvolkov@gmail.com>
2025-11-30 17:26:20 +0300

    fix: update base path in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  base: '/webchat/',
+  base: '/',
   plugins: [
     vue({
       template: {
